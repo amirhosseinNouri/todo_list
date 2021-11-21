894d9580cb2cffbda237649bb2131f6e74e04867
Amirhossein Nouri <amirhossein.nouri@snapp.cab>
2021-11-21 10:56:22 +0330

    feat: improve service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,27 @@
-console.log('Hello from service worker');
+const assets = [
+  '/src',
+  '/src/fonts/Montserrat-Regular.otf',
+  '/src/style.css',
+  '/src/app.js',
+  '/src/sw-register.js',
+];
+
+self.addEventListener('install', (e) => {
+  caches.open('assets').then((cache) => {
+    cache.addAll(assets);
+  });
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.open('assets').then((cache) => {
+      cache.match(e.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        } else {
+          return fetch(e.request.url);
+        }
+      });
+    }),
+  );
+});
